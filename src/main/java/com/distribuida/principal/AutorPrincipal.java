8a595336513109f79db6a458332016aab269a933
CV <cvilla@tecnologicoismac.edu.ec>
2025-05-16 12:25:17 -0500

    clases libro. factura

---
+++ src/main/java/com/distribuida/principal/AutorPrincipal.java
@@ -0,0 +1,17 @@
+package com.distribuida.principal;
+
+import com.distribuida.entities.Autor;
+
+public class AutorPrincipal {
+
+    public static void main(String[] args){
+        Autor autor = new Autor(1,"Enzo","Fernandez","Argentina","La Boca","566","enzofer@gmai.com");
+
+        autor.setNombre();
+
+
+    }
+
+
+
+}
